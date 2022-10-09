2815c50b68dd02b3b2eaf72f384842252d2d97cd
DKhoi02 <khoidndgcc200058@fpt.edu.vn>
2022-10-09 16:59:22 +0700

    commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '090cfc632d0b59f9756f95e06608593cdc3fe92c',
+        'reference' => '7b2b04e22348c628e4c238ad1bb5daf40d589e0d',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '090cfc632d0b59f9756f95e06608593cdc3fe92c',
+            'reference' => '7b2b04e22348c628e4c238ad1bb5daf40d589e0d',
             'dev_requirement' => false,
         ),
         'doctrine/annotations' => array(
